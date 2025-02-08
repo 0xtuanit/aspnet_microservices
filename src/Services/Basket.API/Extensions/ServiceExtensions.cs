69cd3fd417b2bce2dd89fb03e2ab1b8ded4ecef0
haohmaru3000 <tuanit168@gmail.com>
2025-02-08 18:19:14 +0700

    3. Configure Redis service so that Basket.API service can use it

--- src/Services/Basket.API/Extensions/ServiceExtensions.cs
+++ src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -10,5 +10,19 @@ namespace Basket.API.Extensions
         public static IServiceCollection ConfigureServices(this IServiceCollection services) =>
             services.AddScoped<IBasketRepository, BasketRepository>()
                 .AddTransient<ISerializeService, SerializeService>();
+
+
+        public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisConnectionString = configuration.GetSection("CacheSettings:ConnectionString").Value;
+            if (string.IsNullOrEmpty(redisConnectionString))
+                throw new ArgumentNullException("Redis Connection string is not configured");
+
+            //Redis Configuration
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = redisConnectionString;
+            });
+        }
     }
 }
